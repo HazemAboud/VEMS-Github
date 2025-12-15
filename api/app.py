ecf42c08623a8ab9b1e099331c6793f98a9ed3e3
HazemAboud <hazemaboud2005@gmail.com>
2025-12-15 17:58:05 +0800

    initial commit

---
+++ api/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    # Get JSON data from request
+    data = request.get_json()
+
+    email = data.get('email')
+    password = data.get('password')
+    
+    #db logic
+    
+@app.route('/api/login', methods=['POST'])
+def login():
+    # Get JSON data from request
+    data = request.get_json()
+
+    name = data.get('name')
+    email = data.get('email')
+    id = data.get('id')
+    password = data.get('password')
+
+    #db logic
+    
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
